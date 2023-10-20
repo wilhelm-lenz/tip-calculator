5f2edf5ae7801ac93c60add4f0ab42716658cdec
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2023-10-20 17:32:16 +0200

    added another part of project

--- Trinkgeldrechner/assets/js/main.js
+++ Trinkgeldrechner/assets/js/main.js
@@ -1,6 +1,7 @@
 const billElement = document.body.querySelector("#bill");
 const peopleNumberElement = document.body.querySelector("#peoples");
 const serviceElement = document.body.querySelector("#service");
+const outputElement = document.body.querySelector(".output");
 
 console.log(billElement);
 console.log(peopleNumberElement);
@@ -11,11 +12,35 @@ function calculateTip() {
   let peopleNumber = Number(peopleNumberElement.value);
   let serviceLength = serviceElement.options.selectedIndex;
   let service = serviceElement.options[serviceLength].innerText;
-
-  console.log(bill);
-  console.log(peopleNumber);
-  console.log(serviceLength);
-  console.log(service);
+  let output = outputElement;
+  if (service === "schlecht") {
+    output.innerHTML = `<p>Das Trinkgeld ist: ${bill * 0.02} €</p>`;
+    output.innerHTML += `<p> Die Gesammtsumme beträgt: ${
+      bill + bill * 0.02
+    } €</p>`;
+    output.innerHTML += `<p>Der Preis pro Person ist ${
+      (bill + bill * 0.02) / peopleNumber
+    } €</p>`;
+  } else if (service === "ok") {
+    output.innerHTML = `<p>Das Trinkgeld ist: ${bill * 0.1} €</p>`;
+    output.innerHTML += `<p> Die Gesammtsumme beträgt: ${
+      bill + bill * 0.1
+    } €</p>`;
+    output.innerHTML += `<p>Der Preis pro Person ist ${
+      (bill + bill * 0.1) / peopleNumber
+    } €</p>`;
+  } else if (service === "gut") {
+    output.innerHTML = `<p>Das Trinkgeld ist: ${bill * 0.2} €</p>`;
+    output.innerHTML += `<p> Die Gesammtsumme beträgt: ${
+      bill + bill * 0.2
+    } €</p>`;
+    output.innerHTML += `<p>Der Preis pro Person ist ${
+      (bill + bill * 0.2) / peopleNumber
+    } €</p>`;
+  }
+  console.log(bill * 0.2);
+  console.log(bill + bill * 0.2);
+  console.log((bill + bill * 0.2) / peopleNumber);
 }
 
 // const bill = 275;
