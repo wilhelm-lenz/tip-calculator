fc29ad39c7a4fcd9118facbf8ca8c02d71c3c795
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2023-10-20 20:27:39 +0200

    make refactoring

--- Trinkgeldrechner/assets/js/main.js
+++ Trinkgeldrechner/assets/js/main.js
@@ -2,53 +2,45 @@ const billElement = document.body.querySelector("#bill");
 const peopleNumberElement = document.body.querySelector("#peoples");
 const serviceElement = document.body.querySelector("#service");
 const outputElement = document.body.querySelector(".output");
+let output = outputElement;
+const percentOfTip = {
+  schlecht: 0.02,
+  ok: 0.1,
+  gut: 0.2,
+};
+const servicePerformance = ["schlecht", "ok", "gut", "Wie war der Service"];
+const textAmounts = [
+  "Das Trinkgeld ist:",
+  "Die Gesammtsumme beträgt:",
+  "Der Preis pro Person ist:",
+];
 
-console.log(billElement);
-console.log(peopleNumberElement);
-console.log(serviceElement);
-
-function calculateTip() {
+function showAmounts(service) {
   let bill = Number(billElement.value);
   let peopleNumber = Number(peopleNumberElement.value);
-  let serviceLength = serviceElement.options.selectedIndex;
-  let service = serviceElement.options[serviceLength].innerText;
-  let output = outputElement;
-  if (service === "schlecht") {
-    output.innerHTML = `<p>Das Trinkgeld ist: ${bill * 0.02} €</p>`;
-    output.innerHTML += `<p> Die Gesammtsumme beträgt: ${
-      bill + bill * 0.02
-    } €</p>`;
-    output.innerHTML += `<p>Der Preis pro Person ist ${
-      (bill + bill * 0.02) / peopleNumber
-    } €</p>`;
-  } else if (service === "ok") {
-    output.innerHTML = `<p>Das Trinkgeld ist: ${bill * 0.1} €</p>`;
-    output.innerHTML += `<p> Die Gesammtsumme beträgt: ${
-      bill + bill * 0.1
-    } €</p>`;
-    output.innerHTML += `<p>Der Preis pro Person ist ${
-      (bill + bill * 0.1) / peopleNumber
-    } €</p>`;
-  } else if (service === "gut") {
-    output.innerHTML = `<p>Das Trinkgeld ist: ${bill * 0.2} €</p>`;
-    output.innerHTML += `<p> Die Gesammtsumme beträgt: ${
-      bill + bill * 0.2
-    } €</p>`;
-    output.innerHTML += `<p>Der Preis pro Person ist ${
-      (bill + bill * 0.2) / peopleNumber
-    } €</p>`;
-  }
-  console.log(bill * 0.2);
-  console.log(bill + bill * 0.2);
-  console.log((bill + bill * 0.2) / peopleNumber);
+  output.innerHTML = `<p>${textAmounts[0]} ${(
+    bill * percentOfTip[service]
+  ).toFixed(2)} €</p>`;
+  output.innerHTML += `<p>${textAmounts[1]}  ${(
+    bill +
+    bill * percentOfTip[service]
+  ).toFixed(2)} €</p>`;
+  output.innerHTML += `<p>${textAmounts[2]} ${(
+    (bill + bill * percentOfTip[service]) /
+    peopleNumber
+  ).toFixed(2)} €</p>`;
 }
 
-// const bill = 275;
-
-/* Write your code below. Good luck! 🙂 */
-
-// const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
-
-// console.log(
-//   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
-// );
+function calculateTip() {
+  let serviceLength = serviceElement.options.selectedIndex;
+  let service = serviceElement.options[serviceLength].innerText;
+  if (service === servicePerformance[0]) {
+    showAmounts(service);
+  } else if (service === servicePerformance[1]) {
+    showAmounts(service);
+  } else if (service === servicePerformance[2]) {
+    showAmounts(service);
+  } else if (service === servicePerformance[3]) {
+    output.innerHTML = "<p>Bitte Service bewerten!</p>";
+  }
+}
